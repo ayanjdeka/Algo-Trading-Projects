e815d49e086cd5f6e6f9e1591a80585e395fea04
ayanjdeka <ayanjdeka@gmail.com>
2022-05-26 00:39:59 -0400

    Adding Sophisticated Strategies

---
+++ SophisticatedAlgorithmStrategies/MeanReversionStrategy/MeanReversionStrategy.py
@@ -0,0 +1,176 @@
+import pandas as pd
+from pandas_datareader import data
+
+# Fetch daily data for 4 years
+SYMBOL='GOOG'
+start_date = '2014-01-01'
+end_date = '2018-01-01'
+SRC_DATA_FILENAME=SYMBOL + '_data.pkl'
+
+try:
+  data = pd.read_pickle(SRC_DATA_FILENAME)
+except FileNotFoundError:
+  data = data.DataReader(SYMBOL, 'yahoo', start_date, end_date)
+  data.to_pickle(SRC_DATA_FILENAME)
+
+NUM_PERIODS_FAST = 20
+K_FAST = 2 / (NUM_PERIODS_FAST +1)
+ema_fast = 0
+ema_fast_values = []
+
+NUM_PERIODS_SLOW = 40
+K_SLOW = 2 / (NUM_PERIODS_SLOW + 1) 
+ema_slow_values = [] 
+
+apo_values = [] 
+
+orders = [] #tracking buy/sell order, +1 for a buy order, and -1 for a sell order
+positions = [] #tracks positions, +ve for long positions, -ve for short, and 0 for flat positions
+pnls = []
+
+last_buy_price = 0 
+last_sell_price = 0 
+position = 0 
+buy_sum_price_qty = 0 
+buy_sum_qty = 0 
+sell_sum_price_qty = 0 
+sell_sum_qty = 0
+open_pnl = 0 
+closed_pnl = 0 
+
+
+# Constants that define strategy behavior/thresholds
+APO_VALUE_FOR_BUY_ENTRY = -10 # APO trading signal value below which to enter buy-orders/long-position
+APO_VALUE_FOR_SELL_ENTRY = 10 # APO trading signal value above which to enter sell-orders/short-position
+MIN_PRICE_MOVE_FROM_LAST_TRADE = 10 # Minimum price change since last trade before considering trading again, this is to prevent over-trading at/around same prices
+NUM_SHARES_PER_TRADE = 10 # Number of shares to buy/sell on every trade
+MIN_PROFIT_TO_CLOSE = 10*NUM_SHARES_PER_TRADE # Minimum Open/Unrealized profit at which to close positions and lock profits
+
+import statistics as stats
+import math as math
+
+SMA_NUM_PERIODS = 20
+price_history = []
+close = data['Close']
+
+for close_price in close:
+    if (ema_fast == 0): # first observation
+        ema_fast = close_price
+        ema_slow = close_price
+    else:
+        ema_fast = (close_price - ema_fast) * K_FAST + ema_fast
+        ema_slow = (close_price - ema_slow) * K_SLOW + ema_slow
+
+    ema_fast_values.append(ema_fast)
+    ema_slow_values.append(ema_slow)
+
+    apo = ema_fast - ema_slow
+    apo_values.append(apo)
+
+    # We will perform a sell trade at close_price if the following conditions are met:
+    # 1. The APO trading signal value is above the threshold for sell-entry and the difference between last trade-price and current-price is enough
+    # 2. We are long position and either APO trading signal value is >=0  or current position is profitable
+    if ((apo > APO_VALUE_FOR_SELL_ENTRY and abs(close_price - last_sell_price) > MIN_PRICE_MOVE_FROM_LAST_TRADE) # APO above sell entry threshold, we should sell
+    or
+    (position > 0 and (apo >= 0 or open_pnl > MIN_PROFIT_TO_CLOSE))): # long  and APO has gone positive, sell to close position
+        orders.append(-1) # mark the sell trade
+        last_sell_price = close_price
+        position -= NUM_SHARES_PER_TRADE # reduce position by the size of this trade
+        sell_sum_price_qty += (close_price*NUM_SHARES_PER_TRADE) 
+        sell_sum_qty += NUM_SHARES_PER_TRADE
+        print( "Sell ", NUM_SHARES_PER_TRADE, " @ ", close_price, "Position: ", position )
+
+    elif ((apo < APO_VALUE_FOR_BUY_ENTRY and abs(close_price - last_buy_price) > MIN_PRICE_MOVE_FROM_LAST_TRADE) # APO below buy entry threshold, we should buy
+    or
+    (position < 0 and (apo <= 0 or open_pnl > MIN_PROFIT_TO_CLOSE))): # short and APO has gone negative, buy to close position
+        orders.append(+1) # mark the buy trade
+        last_buy_price = close_price
+        position += NUM_SHARES_PER_TRADE # increase position by the size of this trade
+        buy_sum_price_qty += (close_price*NUM_SHARES_PER_TRADE) 
+        buy_sum_qty += NUM_SHARES_PER_TRADE
+        print( "Buy ", NUM_SHARES_PER_TRADE, " @ ", close_price, "Position: ", position )
+    else:
+        # No trade since none of the conditions were met to buy or sell
+        orders.append(0)
+
+    positions.append(position)
+
+    #update Open/Unrealized & Closed/Realized positions
+
+  # This section updates Open/Unrealized & Closed/Realized positions
+    open_pnl = 0
+    if position > 0:
+        if sell_sum_qty > 0: # long position and some sell trades have been made against it, close that amount based on how much was sold against this long position
+            open_pnl = abs(sell_sum_qty) * (sell_sum_price_qty/sell_sum_qty - buy_sum_price_qty/buy_sum_qty)
+            # mark the remaining position to market i.e. pnl would be what it would be if we closed at current price
+        open_pnl += abs(sell_sum_qty - position) * (close_price - buy_sum_price_qty / buy_sum_qty)
+    elif position < 0:
+        if buy_sum_qty > 0: # short position and some buy trades have been made against it, close that amount based on how much was bought against this short position
+            open_pnl = abs(buy_sum_qty) * (sell_sum_price_qty/sell_sum_qty - buy_sum_price_qty/buy_sum_qty)
+        # mark the remaining position to market i.e. pnl would be what it would be if we closed at current price
+        open_pnl += abs(buy_sum_qty - position) * (sell_sum_price_qty/sell_sum_qty - close_price)
+    else:
+        # flat, so update closed_pnl and reset tracking variables for positions & pnls
+        closed_pnl += (sell_sum_price_qty - buy_sum_price_qty)
+        buy_sum_price_qty = 0
+        buy_sum_qty = 0
+        sell_sum_price_qty = 0
+        sell_sum_qty = 0
+        last_buy_price = 0
+        last_sell_price = 0
+
+    print( "OpenPnL: ", open_pnl, " ClosedPnL: ", closed_pnl, " TotalPnL: ", (open_pnl + closed_pnl) )
+    pnls.append(closed_pnl + open_pnl)
+
+# This section prepares the dataframe from the trading strategy results and visualizes the results
+data = data.assign(ClosePrice=pd.Series(close, index=data.index))
+data = data.assign(Fast10DayEMA=pd.Series(ema_fast_values, index=data.index))
+data = data.assign(Slow40DayEMA=pd.Series(ema_slow_values, index=data.index))
+data = data.assign(APO=pd.Series(apo_values, index=data.index))
+data = data.assign(Trades=pd.Series(orders, index=data.index))
+data = data.assign(Position=pd.Series(positions, index=data.index))
+data = data.assign(Pnl=pd.Series(pnls, index=data.index))
+
+import matplotlib.pyplot as plt
+
+data['ClosePrice'].plot(color='blue', lw=1., legend=True)
+data['Fast10DayEMA'].plot(color='y', lw=1., legend=True)
+data['Slow40DayEMA'].plot(color='m', lw=1., legend=True)
+plt.plot(data.loc[ data.Trades == 1 ].index, data.ClosePrice[data.Trades == 1 ], color='r', lw=0, marker='^', markersize=3, label='buy')
+plt.plot(data.loc[ data.Trades == -1 ].index, data.ClosePrice[data.Trades == -1 ], color='g', lw=0, marker='v', markersize=3, label='sell')
+plt.legend()
+plt.show()
+
+data['APO'].plot(color='k', lw=3., legend=True)
+plt.plot(data.loc[ data.Trades == 1 ].index, data.APO[data.Trades == 1 ], color='r', lw=0, marker='^', markersize=7, label='buy')
+plt.plot(data.loc[ data.Trades == -1 ].index, data.APO[data.Trades == -1 ], color='g', lw=0, marker='v', markersize=7, label='sell')
+plt.axhline(y=0, lw=0.5, color='k')
+for i in range( APO_VALUE_FOR_BUY_ENTRY, APO_VALUE_FOR_BUY_ENTRY*5, APO_VALUE_FOR_BUY_ENTRY ):
+  plt.axhline(y=i, lw=0.5, color='r')
+for i in range( APO_VALUE_FOR_SELL_ENTRY, APO_VALUE_FOR_SELL_ENTRY*5, APO_VALUE_FOR_SELL_ENTRY ):
+  plt.axhline(y=i, lw=0.5, color='g')
+plt.legend()
+plt.show()
+
+data['Position'].plot(color='k', lw=1., legend=True)
+plt.plot(data.loc[ data.Position == 0 ].index, data.Position[ data.Position == 0 ], color='k', lw=0, marker='.', label='flat')
+plt.plot(data.loc[ data.Position > 0 ].index, data.Position[ data.Position > 0 ], color='r', lw=0, marker='+', label='long')
+plt.plot(data.loc[ data.Position < 0 ].index, data.Position[ data.Position < 0 ], color='g', lw=0, marker='_', label='short')
+plt.axhline(y=0, lw=0.5, color='k')
+for i in range( NUM_SHARES_PER_TRADE, NUM_SHARES_PER_TRADE*25, NUM_SHARES_PER_TRADE*5 ):
+  plt.axhline(y=i, lw=0.5, color='r')
+for i in range( -NUM_SHARES_PER_TRADE, -NUM_SHARES_PER_TRADE*25, -NUM_SHARES_PER_TRADE*5 ):
+  plt.axhline(y=i, lw=0.5, color='g')
+plt.legend()
+plt.show()
+
+data['Pnl'].plot(color='k', lw=1., legend=True)
+plt.plot(data.loc[ data.Pnl > 0 ].index, data.Pnl[ data.Pnl > 0 ], color='g', lw=0, marker='.')
+plt.plot(data.loc[ data.Pnl < 0 ].index, data.Pnl[ data.Pnl < 0 ], color='r', lw=0, marker='.')
+plt.legend()
+plt.show()
+
+data.to_csv("basic_mean_reversion.csv", sep=",")
+
+
+
