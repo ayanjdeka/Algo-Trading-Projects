5ade73fbdd758a4462fbcbcb05ae0e0b51af2318
ayanjdeka <ayanjdeka@gmail.com>
2023-06-25 19:35:12 -0500

    Adding more custom pair trading strategy with the Ridge Regression

--- PairTradingStrategies/RidgeWithPairsTrading.py
+++ PairTradingStrategies/RidgeWithPairsTrading.py
@@ -1,5 +1,4 @@
-from cProfile import label
-from logging import warn
+from sklearn.linear_model import Ridge
 from sklearn.linear_model import LinearRegression
 import warnings
 import pandas as pd
@@ -11,12 +10,12 @@ end_date = '2024-01-01'
 aapl_data_frame = yf.download('AAPL', start = start_date, end = end_date)
 google_data_frame = yf.download('GOOG', start = start_date, end = end_date)
 
-class LinearRegressionModel(object):
+class RidgeRegressionModel(object):
     def __init__(self):
         self.df_result = pd.DataFrame(columns=['Actual', 'Predicted'])
 
     def get_model(self):
-        return LinearRegression(fit_intercept=False)
+        return Ridge(alpha=0.5)
 
     def learn(self, df, ys, start_date, end_date, lookback_period=20):
         model = self.get_model()
@@ -40,20 +39,20 @@ class LinearRegressionModel(object):
     def get_prices_since(self, df, date_since, lookback):
         index = df.index.get_loc(date_since)
         return df.iloc[index-lookback:index]
+    
 
-df_x = pd.DataFrame({'AAPL': aapl_data_frame['Adj Close']})
-goog_prices = google_data_frame['Adj Close']
+
 
 df_x = pd.DataFrame({'GOOG': google_data_frame['Adj Close']})
 apple_prices = aapl_data_frame['Adj Close']
 
-linear_reg_model = LinearRegressionModel()
-linear_reg_model.learn(df_x, apple_prices, start_date='2020', 
+ridge_reg_model = RidgeRegressionModel()
+ridge_reg_model.learn(df_x, apple_prices, start_date='2020', 
                        end_date='2022', lookback_period=20)
 
 plt.figure(figsize=(12, 8))
-plt.plot(linear_reg_model.df_result['Actual'], label= 'Apple Actual Returns')
-plt.plot(linear_reg_model.df_result['Predicted'], label = 'Strategy Returns')
+plt.plot(ridge_reg_model.df_result['Actual'], label= 'Apple Actual Returns')
+plt.plot(ridge_reg_model.df_result['Predicted'], label = 'Strategy Returns')
 plt.legend()
 plt.title('Apple Returns Trained on Google Data and Using Ridge')
 plt.show()
